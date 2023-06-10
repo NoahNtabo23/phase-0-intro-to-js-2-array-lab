fcab6a99502c373c3358703aa8ed5ae225973350
NoahNtabo23 <135032672+NoahNtabo23@users.noreply.github.com>
2023-06-11 00:11:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+console.log(cats)
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+    
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift()
+}
+function appendCat(name){
+    return cats.concat(name)
+}
+const newArray=append.Cat("Broom")
+
+function prependCat(name){
+    return [name].concat(cats)
+}
+function removeLastCat(name){
+    return cats.slice(0,-1)
+}
+
+
+
+
+  
+
+
+
